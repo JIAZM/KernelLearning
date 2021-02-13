9e0870a82c9982cae57f2f7ea0fa94c4c1efd305
Jiazm <jiazm556@163.com>
2021-02-13 16:18:35 +0800

    使用Makefile更新隐藏文件

--- printpcb/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/thirty/code/kernellearn/printpcb/modules.order := {   echo /home/thirty/code/kernellearn/printpcb/pcbindex.ko; :; } | awk '!x[$$0]++' - > /home/thirty/code/kernellearn/printpcb/modules.order
